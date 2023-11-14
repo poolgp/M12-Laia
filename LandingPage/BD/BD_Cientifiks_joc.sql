5162f0368cc3dc30c54582ea9e6079fdc37b1ece
poolgp <145465364+poolgp@users.noreply.github.com>
2023-11-14 15:37:47 +0100

    14/11/2023

--- LandingPage/BD/BD_Cientifiks_joc.sql
+++ LandingPage/BD/BD_Cientifiks_joc.sql
@@ -1,7 +1,8 @@
 create database if not exists BD_Cientifiks_joc;
 
 create table if not exists usuaris(
-idJugador int (30) auto_increment,
-nom varchar (30) not null unique,
-contrasenya varchar(255) not null
+    idJugador int (30) auto_increment,
+    nom varchar (30) not null unique,
+    contrasenya varchar(255) not null
 );
+
