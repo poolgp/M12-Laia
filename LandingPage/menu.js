6f2472b295ddd3d24d05a0e2cf2e4898e179a137
filiprendon <frendont2223@politecnics.barcelona>
2023-11-27 16:53:15 +0100

    27/11/2023 Landing

--- LandingPage/menu.js
+++ LandingPage/menu.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".menu");
+    const linesContainer = document.getElementById("lines");
+    const points = document.querySelectorAll(".point");
 
-    menuToggle.addEventListener("click", function () {
-        menu.classList.toggle("show-menu");
-    });
+    // Conecta los puntos con líneas discontinuas
+    function connectPoints() {
+        for (let i = 0; i < points.length; i++) {
+            for (let j = i + 1; j < points.length; j++) {
+                const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                line.setAttribute("x1", points[i].offsetLeft + 15);
+                line.setAttribute("y1", points[i].offsetTop + 15);
+                line.setAttribute("x2", points[j].offsetLeft + 15);
+                line.setAttribute("y2", points[j].offsetTop + 15);
+                line.setAttribute("stroke", "#3498db");
+                line.setAttribute("stroke-dasharray", "5,5");
+                linesContainer.appendChild(line);
+            }
+        }
+    }
+
+    connectPoints();
 });
