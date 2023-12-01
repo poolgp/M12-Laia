bd8749774d7e8ed5d49cee14015e94fe4ff43d69
poolgp <145465364+poolgp@users.noreply.github.com>
2023-12-01 13:11:32 +0100

    01/12/2023

--- LandingPage/LandingPage.js
+++ LandingPage/LandingPage.js
@@ -3,9 +3,9 @@ var tooltips = document.querySelectorAll('.tooltip');
 
 window.onmousemove = function (e) {
   var x = e.clientX,
-      y = e.clientY;
-  
-  tooltips.forEach(function(tooltip) {
+    y = e.clientY;
+
+  tooltips.forEach(function (tooltip) {
     tooltip.style.top = (y + 20) + 'px';
     tooltip.style.left = (x + 20) + 'px';
   });
