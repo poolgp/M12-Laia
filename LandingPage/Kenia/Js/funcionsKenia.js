fdf0f6e09d132151fb417a7f339f3fdd5283a728
RogerOrti <rortim2223@politecnics.barcelona>
2023-12-13 19:55:43 +0100

    13/12/23 Kenia

--- LandingPage/Kenia/Js/funcionsKenia.js
+++ LandingPage/Kenia/Js/funcionsKenia.js
@@ -1,96 +1,96 @@
-document.addEventListener("DOMContentLoaded", pintarMascara);
+// document.addEventListener("DOMContentLoaded", pintarMascara);
 
-let arrayColors = [];
-let colorSeleccionat = null;
-
-
-let mascara = getElementById("mascaras");
-
-mascara.addEventListener("click", function() {
-  // Cambiar el color al azul al hacer clic
-  miImagen.style.fill = "blue";
-});
+// let arrayColors = [];
+// let colorSeleccionat = null;
 
 
-function pintarMascara() {
-  let colorFons1 = document.getElementById("celda1");
-  colorFons1.style.backgroundColor = "orange";
-  arrayColors.push("orange");
+// let mascara = getElementById("mascaras");
 
-  let colorFons2 = document.getElementById("celda2");
-  colorFons2.style.backgroundColor = "blue";
-  arrayColors.push("blue");
-
-  let colorFons3 = document.getElementById("celda3");
-  colorFons3.style.backgroundColor = "green";
-  arrayColors.push("green");
-
-  let colorFons4 = document.getElementById("celda4");
-  colorFons4.style.backgroundColor = "red";
-  arrayColors.push("red");
-
-  const myTimeout = setTimeout(amagarMascara, 5000);
-}
-
-function amagarMascara() {
-  let colorFons1 = document.getElementById("celda1");
-  colorFons1.style.backgroundColor = "white";
-
-  let colorFons2 = document.getElementById("celda2");
-  colorFons2.style.backgroundColor = "white";
-
-  let colorFons3 = document.getElementById("celda3");
-  colorFons3.style.backgroundColor = "white";
-
-  let colorFons4 = document.getElementById("celda4");
-  colorFons4.style.backgroundColor = "white";
-}
-
-function colorPinzell(div) {
-  // Obtener el color del div clickeado
-  colorSeleccionat = window.getComputedStyle(div).getPropertyValue("background-color");
-  console.log("Color seleccionado:", colorSeleccionat);
-}
-
-function pintarCuadrado(event) {
-  if (colorSeleccionat) {
-    // Pintar el cuadrado con el color seleccionado
-    event.target.style.backgroundColor = colorSeleccionat;
-  } else {
-    console.log("Selecciona un color primero");
-  }
-}
+// mascara.addEventListener("click", function() {
+//   // Cambiar el color al azul al hacer clic
+//   miImagen.style.fill = "blue";
+// });
 
 
-function compararMascaras() {
-  let colorMascaraPredeterminada1 = window.getComputedStyle(document.getElementById("celda1")).getPropertyValue("background-color");
-  let colorMascaraPredeterminada2 = window.getComputedStyle(document.getElementById("celda2")).getPropertyValue("background-color");
-  let colorMascaraPredeterminada3 = window.getComputedStyle(document.getElementById("celda3")).getPropertyValue("background-color");
-  let colorMascaraPredeterminada4 = window.getComputedStyle(document.getElementById("celda4")).getPropertyValue("background-color");
+// function pintarMascara() {
+//   let colorFons1 = document.getElementById("celda1");
+//   colorFons1.style.backgroundColor = "orange";
+//   arrayColors.push("orange");
 
-  let arrayColoresMascaraPredeterminada = [colorMascaraPredeterminada1, colorMascaraPredeterminada2, colorMascaraPredeterminada3, colorMascaraPredeterminada4];
+//   let colorFons2 = document.getElementById("celda2");
+//   colorFons2.style.backgroundColor = "blue";
+//   arrayColors.push("blue");
 
-  let arrayColoresMascaraUsuario = [];
-  for (let i = 1; i <= 4; i++) {
-    let color = window.getComputedStyle(document.getElementById("celda" + i)).getPropertyValue("background-color");
-    arrayColoresMascaraUsuario.push(color);
-  }
+//   let colorFons3 = document.getElementById("celda3");
+//   colorFons3.style.backgroundColor = "green";
+//   arrayColors.push("green");
 
-  let sonIguales = true;
-  for (let i = 0; i < arrayColoresMascaraPredeterminada.length; i++) {
-    if (arrayColoresMascaraPredeterminada[i] !== arrayColoresMascaraUsuario[i]) {
-      sonIguales = false;
-      break;
-    }
-  }
+//   let colorFons4 = document.getElementById("celda4");
+//   colorFons4.style.backgroundColor = "red";
+//   arrayColors.push("red");
 
-  let resultadoElemento = document.getElementById("resultadoComparacion");
-  if (sonIguales) {
-    resultadoElemento.textContent = "¡Máscaras iguales!";
-  } else {
-    resultadoElemento.textContent = "Las máscaras no son iguales.";
-  }
-} 
+//   const myTimeout = setTimeout(amagarMascara, 5000);
+// }
+
+// function amagarMascara() {
+//   let colorFons1 = document.getElementById("celda1");
+//   colorFons1.style.backgroundColor = "white";
+
+//   let colorFons2 = document.getElementById("celda2");
+//   colorFons2.style.backgroundColor = "white";
+
+//   let colorFons3 = document.getElementById("celda3");
+//   colorFons3.style.backgroundColor = "white";
+
+//   let colorFons4 = document.getElementById("celda4");
+//   colorFons4.style.backgroundColor = "white";
+// }
+
+// function colorPinzell(div) {
+//   // Obtener el color del div clickeado
+//   colorSeleccionat = window.getComputedStyle(div).getPropertyValue("background-color");
+//   console.log("Color seleccionado:", colorSeleccionat);
+// }
+
+// function pintarCuadrado(event) {
+//   if (colorSeleccionat) {
+//     // Pintar el cuadrado con el color seleccionado
+//     event.target.style.backgroundColor = colorSeleccionat;
+//   } else {
+//     console.log("Selecciona un color primero");
+//   }
+// }
+
+
+// function compararMascaras() {
+//   let colorMascaraPredeterminada1 = window.getComputedStyle(document.getElementById("celda1")).getPropertyValue("background-color");
+//   let colorMascaraPredeterminada2 = window.getComputedStyle(document.getElementById("celda2")).getPropertyValue("background-color");
+//   let colorMascaraPredeterminada3 = window.getComputedStyle(document.getElementById("celda3")).getPropertyValue("background-color");
+//   let colorMascaraPredeterminada4 = window.getComputedStyle(document.getElementById("celda4")).getPropertyValue("background-color");
+
+//   let arrayColoresMascaraPredeterminada = [colorMascaraPredeterminada1, colorMascaraPredeterminada2, colorMascaraPredeterminada3, colorMascaraPredeterminada4];
+
+//   let arrayColoresMascaraUsuario = [];
+//   for (let i = 1; i <= 4; i++) {
+//     let color = window.getComputedStyle(document.getElementById("celda" + i)).getPropertyValue("background-color");
+//     arrayColoresMascaraUsuario.push(color);
+//   }
+
+//   let sonIguales = true;
+//   for (let i = 0; i < arrayColoresMascaraPredeterminada.length; i++) {
+//     if (arrayColoresMascaraPredeterminada[i] !== arrayColoresMascaraUsuario[i]) {
+//       sonIguales = false;
+//       break;
+//     }
+//   }
+
+//   let resultadoElemento = document.getElementById("resultadoComparacion");
+//   if (sonIguales) {
+//     resultadoElemento.textContent = "¡Máscaras iguales!";
+//   } else {
+//     resultadoElemento.textContent = "Las máscaras no son iguales.";
+//   }
+// } 
 
 
 
@@ -145,3 +145,27 @@ function compararMascaras() {
 //   // Aquí puedes realizar acciones adicionales basadas en la respuesta elegida
 // }
 
+
+  function CambiarColor(obj, color) {
+
+    //window.alert(obj + " " + color);
+
+    var frente = document.getElementById("mascara").getSVGDocument().getElementById(obj);
+    frente.style.setProperty("fill", color, "");
+
+    //    var ulls1 = document.getElementById("mascara").getSVGDocument().getElementById("ulls1");
+    //  ulls1.style.setProperty("fill","red", "");
+
+    //    var ulls2 = document.getElementById("mascara").getSVGDocument().getElementById("ulls2");
+    //    ulls2.style.setProperty("fill","red", "");
+  }
+
+  function openPopup() {
+    console.log("Opening popup");
+    document.getElementById('infoPopup').style.display = 'block';
+  }
+
+  function closePopup() {
+    console.log("Closing popup");
+    document.getElementById('infoPopup').style.display = 'none';
+  }
